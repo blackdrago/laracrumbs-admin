4fa0206c9bca6adf0c6afb2043ccdd4e2835795a
K. McCormick <kyliemccormick@gmail.com>
2018-05-27 17:34:26 -0400

    [Laracrumb Admin] Typo fix.

--- src/config/constants.php
+++ src/config/constants.php
@@ -5,5 +5,5 @@
  * @package LaracrumbsAdmin
  */
 
-/** @const LARACRUMB_NOLINK_PREFIX        The prefix assigned to non-link laracrumbs for links. */
-const LARACRUMB_NOLINK_PREFIX = '_~_NOLINK_~_';
+/** @const NEW_LARACRUMB_NOLINK_PREFIX        The prefix assigned to non-link laracrumbs for links. */
+const NEW_LARACRUMB_NOLINK_PREFIX = '_~_NOLINK_~_';
